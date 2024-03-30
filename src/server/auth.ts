d9582cc3530e003e859286419f16f7189b905dfb
ravvi-kumar <ravvi.kumar8989@gmail.com>
2024-03-30 21:15:41 +0530

    fix : removed unnecessary import

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -9,7 +9,6 @@ import DiscordProvider from "next-auth/providers/discord";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
-import { createTable } from "~/server/db/schema";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -47,7 +46,7 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   },
-  adapter: DrizzleAdapter(db, createTable) as Adapter,
+  adapter: DrizzleAdapter(db) as Adapter,
   providers: [
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
